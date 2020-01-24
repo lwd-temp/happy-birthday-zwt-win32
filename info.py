30a8a9de155fc93bf035c1da83ed48d1f93a0b57
lwd-temp <lwd-temp@github.com>
2020-01-24 19:37:20 +0800

    Publish

---
+++ info.py
@@ -0,0 +1,59 @@
+#coding:utf-8
+# info.py
+# happy-birthday-zwt-win32 的配置文件和字符串常量
+# https://github.com/lwd-temp/happy-birthday-zwt-win32
+# Copyright © 2020 lwd-temp@Github.com. All rights reserved.
+# 注意 所有变量修改后保持原变量类型
+# 日志
+log=1
+# 调试选项 用于pyinstaller测试和控制台输出
+debug=1
+# 字符串常量 见下文
+name="ZWT"
+# 信息内容
+msg="Happy Birthday "+name+"!"
+# 标题 若可能
+title="happy-birthday-zwt-win32"
+# 月
+month=5
+# 日
+day=5
+# 提前生效月
+activemonth=4
+# 提前生效日
+activeday=20
+# 结束月
+disactmonth=5
+# 结束日
+disactday=15
+# 行动时 每日
+acthour=14
+# 行动分 每日
+actmin=30
+# 注意 生效日为闭区间
+# 注意 提前生效月和结束月在程序中视为同一月 配置时必须注意必须填写同一月
+# 这是一个逻辑错误但暂时不具备修改必要
+# 用户附加内容 输出于说明文件 见下文
+usercontent='''这是 happy-birthday-zwt-win32 项目的默认用户附加内容。
+在构建发布版前更改 info.py 以修改此文本。
+https://github.com/lwd-temp/happy-birthday-zwt-win32
+Copyright © 2020 lwd-temp@Github.com. All rights reserved.'''
+# 是否生成说明文件
+createreadme=1
+
+
+
+
+# 简化变量名 便于调用
+actm=activemonth
+actd=activeday
+dism=disactmonth
+disd=disactday
+ah=acthour
+am=actmin
+readme=createreadme
+# 字符串常量和其他说明文本 无需改动
+copyright='''Copyright © 2020 lwd-temp@Github.com. All rights reserved.'''
+aboutwp='''桌面壁纸已经由注册表修改，如需更换请手动修改，壁纸似乎会在重新启动后恢复。'''
+aboutpy='''此应用程序使用Python及pywin32库编写，即使代码风格简单、属于解释型语言运行效率低、学习门槛低，但无论是在大数据、人工智能领域还是用于如此无聊的用途，Python仍然在焕发生机。
+开发者相信Python在未来的20年内仍会保持流行。'''
---
+++ main.py
@@ -0,0 +1,141 @@
+#coding:utf-8
+# main.py
+# happy-birthday-zwt-win32 项目的主程序代码
+# https://github.com/lwd-temp/happy-birthday-zwt-win32
+# Copyright © 2020 lwd-temp@Github.com. All rights reserved.
+import time
+import datetime
+import os
+import popup
+from chwp import chwp
+import info
+import sys
+import win32api,win32con,win32gui
+import random
+
+# 版本号发布之前改掉
+version="beta"
+stcuti=datetime.datetime.now()
+strstcuti=str(stcuti.year)+"-"+str(stcuti.month)+"-"+str(stcuti.day)+"-"+str(stcuti.hour)+"-"+str(stcuti.minute)+"-"+str(stcuti.second)
+logname="hbzwin32log-"+strstcuti+".txt"
+actstat=0 # 布尔 是否激活操作
+statwpact=0
+
+def get_desktop():
+    # 获取桌面路径
+    key =win32api.RegOpenKey(win32con.HKEY_CURRENT_USER,r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders',0,win32con.KEY_READ)
+    return win32api.RegQueryValueEx(key,'Desktop')[0]
+desktop=get_desktop()
+
+def logit(msg):
+    # 日志函数
+    if info.debug==1:
+        print(str(msg))
+    if info.log==1:
+        with open(os.path.join(desktop,logname),"a") as logfile:
+            content="["+str(datetime.datetime.now())+"]"+str(msg)+"\n"
+            logfile.write(content)
+
+logit("日志开始")
+logit("hbzwin32"+"版本"+version)
+logit("日志文件"+logname)
+
+logit("桌面路径"+desktop)
+
+def msgbox(title,content):
+    # win32api弹窗
+    win32api.MessageBox(0,str(content),str(title),win32con.MB_OK)
+
+#生成资源文件目录访问路径
+def resource_path(relative_path):
+    if getattr(sys, 'frozen', False): #是否Bundle Resource
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+def donothing():
+    # 什么都不做
+    logit("什么都不做")
+
+def actmsg():
+    # 行动 弹窗
+    logit("弹窗5次")
+    for i in range(1,6):
+        msgbox(info.title,info.msg)
+
+def actpop():
+    # 行动 气泡
+    logit("持续60s气泡")
+    bubble=popup.MainWindow()
+    for i in range(1,21):
+        bubble.startBubble(info.title,info.msg)
+
+def actwp():
+    # 行动 壁纸
+    logit("更改壁纸")
+    chwp(resource_path(os.path.join("res","wp.jpg")))
+
+# 确定日期时间是否符合info
+nowtime=datetime.datetime.now()
+nowmon=nowtime.month
+nowday=nowtime.day
+nowye=nowtime.year
+# 逻辑不严密 需要重写 暂时符合需求
+if nowmon==info.actm:
+    if nowday>=info.actd:
+        actstat=1
+if nowmon==info.month:
+    if nowday<=info.day:
+        actstat=1
+if nowmon==info.dism:
+    if nowday<=info.disd:
+        actstat=1
+if nowmon>info.actm:
+    if nowmon<info.dism:
+        actstat=1
+if actstat==1:
+    logit("行动开始")
+else:
+    logit("日期未到，程序退出")
+    sys.exit()
+
+# 计算剩余秒
+strthe=str(nowye)+"-"+str(nowmon)+"-"+str(nowday)+" "+str(info.ah)+":"+str(info.am)+":"+"0.0"
+thetime=datetime.datetime.strptime(strthe,"%Y-%m-%d %H:%M:%S.%f")
+logit("预计行动时间"+str(thetime)+"，若已过则推迟至明日")
+delta=(thetime-nowtime).seconds
+logit("倒计时"+str(delta)+"秒")
+time.sleep(delta)
+logit("开始行动")
+
+num=random.randint(0,100)
+# 随机方案
+if num==0:
+    donothing()
+if num>=1:
+    if num<=20:
+        actmsg()
+        statmsgboxact=1
+if num>=21:
+    if num<=70:
+        actpop()
+        statpopact=1
+if num>=71:
+    if num<=100:
+        actwp()
+        statwpact=1
+
+#写说明
+if info.readme==1:
+    logit("写入说明文件")
+    with open(resource_path(os.path.join("res","LICENSE")),"r",encoding="utf-8") as licensefile:
+        license=licensefile.read()
+    readmepath=os.path.join(desktop,"HBZWin32说明文件.txt")
+    with open(readmepath,"a",encoding="utf-8") as fileobj:
+        fileobj.write(info.usercontent+"\n")
+        fileobj.write(info.copyright+"\n")
+        fileobj.write(info.aboutpy+"\n")
+        if statwpact==1:
+            fileobj.write(info.aboutwp+"\n")
+        fileobj.write(license+"\n")
